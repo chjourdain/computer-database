2737754b0065228724b21cb5f045780289830b20
chjourdain <cjourdain@excilys.com>
2016-05-02 17:38:58 +0200

    [FIX] encoding

--- computer-database-webapp/src/main/java/com/excilys/computerdatabase/controller/LoginController.java
+++ computer-database-webapp/src/main/java/com/excilys/computerdatabase/controller/LoginController.java
@@ -1,25 +1,16 @@
 package com.excilys.computerdatabase.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.excilys.computerdatabase.persist.dao.UserDao;
-
-
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     
-    @Autowired
-    private UserDao userDao;
-    
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView get() {
-	System.out.println(userDao.findOne("admin"));
-	
 	return new ModelAndView("login");
 
     }
